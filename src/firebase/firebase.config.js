9b38ee46566be8fb0638f366261d5e304ee3f325
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-04-28 23:58:39 +0600

    hided firebase config keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log('firebase keys', import.meta.env.VITE_PASS)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBriOcy0ZYqNGAEF-tdFNVujAcWSgkyPw",
-  authDomain: "arts-crafts-6e42f.firebaseapp.com",
-  projectId: "arts-crafts-6e42f",
-  storageBucket: "arts-crafts-6e42f.appspot.com",
-  messagingSenderId: "643842369702",
-  appId: "1:643842369702:web:0310b85a0ec64585722c67"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
