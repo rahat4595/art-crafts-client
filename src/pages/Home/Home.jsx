97c3ad46f661cdd0641f47564e4672e5d2708c3c
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-04-30 11:23:31 +0600

    added extra another section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,8 @@ import Banner from "./Banner/Banner";
 import { useEffect, useState } from "react";
 import SingleArt from "../SingleArt/SingleArt";
 import Reviews from "./Reviews";
-import { Bounce,  Fade,  JackInTheBox,  Slide } from "react-awesome-reveal";
+import { Bounce, Fade, JackInTheBox, Slide } from "react-awesome-reveal";
+import Works from "./Works";
 
 
 
@@ -28,7 +29,7 @@ const Home = () => {
         <div>
             <div className="max-w-7xl mx-auto mt-10">
                 <JackInTheBox triggerOnce>
-                <h2 className="text-4xl font-bold text-center ">Explore Arts & Crafts</h2>
+                    <h2 className="text-4xl font-bold text-center ">Explore Arts & Crafts</h2>
                 </JackInTheBox>
             </div>
             {/* banner section */}
@@ -38,11 +39,11 @@ const Home = () => {
 
             <div className="max-w-7xl mx-auto mt-10 px-5">
                 <Slide triggerOnce>
-                <h2 className="text-4xl font-bold text-center">Crafted Items</h2>
+                    <h2 className="text-4xl font-bold text-center">Crafted Items</h2>
                 </Slide>
-                
+
                 <p className="text-center text-lg mt-10 lg:px-52">Discover a world of creativity with our diverse collection of craft and art items, ranging from essentials to specialty supplies, perfect for unleashing your artistic potential</p>
-                
+
             </div>
             {/* crafts section */}
             <div className="max-w-7xl mx-auto md:mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
@@ -51,21 +52,38 @@ const Home = () => {
                 }
 
             </div>
+
             <div className="max-w-7xl mx-auto mt-10 px-5">
-                <Bounce triggerOnce>
-                <h2 className="text-4xl font-bold text-center">What Our Customer Says</h2>
-                </Bounce>
-               
+                <Slide triggerOnce>
+                    <h2 className="text-4xl font-bold text-center">Our Works</h2>
+                </Slide>
+
+                <p className="text-center text-lg mt-10 lg:px-52">
+                    Explore the Essence of Our Creations: A Diverse Showcase Reflecting Creativity, Craftsmanship, and Artistic Expression.</p>
+
             </div>
 
-                {/* reviews section */}
-                <div className="max-w-7xl mx-auto rounded-xl p-2 mt-5">
-                    <Reviews></Reviews>
-                </div>
+
+            {/* our works */}
+            <div className="max-w-7xl mx-auto mt-10">
+                <Works></Works>
+            </div>
+
+            <div className="max-w-7xl mx-auto mt-10 px-5">
+                <Bounce triggerOnce>
+                    <h2 className="text-4xl font-bold text-center">What Our Customer Says</h2>
+                </Bounce>
+
+            </div>
+
+            {/* reviews section */}
+            <div className="max-w-7xl mx-auto rounded-xl p-2 mt-5">
+                <Reviews></Reviews>
+            </div>
 
             <div className="max-w-7xl mx-auto mt-10">
                 <Fade triggerOnce>
-                <h2 className="text-4xl font-bold text-center">Art & Craft Categories</h2>
+                    <h2 className="text-4xl font-bold text-center">Art & Craft Categories</h2>
                 </Fade>
                 <p className="text-center text-lg mt-10 lg:px-52">
                     Explore an Array of Artistic Genres and Craft Categories, Each Offering a Unique Canvas for Your Creative Expression and Exploration.</p>
